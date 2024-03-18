34a05cdd3722c6b49a4193eb508d4f4154024a7b
Daniel <dani3lkamanthi@gmail.com>
2024-03-18 13:19:04 +0300

    Modifies CSS and JS

---
+++ script.js
@@ -0,0 +1,17 @@
+function handleViewportSizeChange() {
+    var viewportWidth = window.innerWidth;
+    
+    if (viewportWidth < 768) {
+        document.body.style.backgroundColor = '#FEF1E6' ;
+    } else if (viewportWidth >= 768 && viewportWidth <= 991) {
+        document.body.style.backgroundColor = '#F9D5A7';
+    } else {
+        
+        document.body.style.backgroundColor = '#FEF1E6';
+    }
+}
+
+
+window.addEventListener('resize', handleViewportSizeChange);
+
+handleViewportSizeChange();
